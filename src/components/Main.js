e202138c45e698ac31b0aaa12c0ece120ceb6eec
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-02-13 18:55:52 +0200

    feat: использовать контекст в Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,23 +2,25 @@ import React from 'react';
 import pencil from '../images/Vector(2).svg';
 import { api } from '../utils/Api';
 import Card from './Card';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
+    const currentUser = React.useContext(CurrentUserContext)
 
-    const [userName, setUserName] = React.useState('')
-    const [userDescription, setUserDescription] = React.useState('')
-    const [userAvatar, setUserAvatar] = React.useState('')
+    // const [userName, setUserName] = React.useState('')
+    // const [userDescription, setUserDescription] = React.useState('')
+    // const [userAvatar, setUserAvatar] = React.useState('')
     const [cards, setCards] = React.useState([])
 
-    React.useEffect(() => {
-        api.getUserInfo()
-            .then((data) => {
-                setUserName(data.name)
-                setUserDescription(data.about)
-                setUserAvatar(data.avatar)
-            })
-            .catch((err) => console.log(err))
-    }, [])
+    // React.useEffect(() => {
+    //     api.getUserInfo()
+    //         .then((data) => {
+    //             setUserName(data.name)
+    //             setUserDescription(data.about)
+    //             setUserAvatar(data.avatar)
+    //         })
+    //         .catch((err) => console.log(err))
+    // }, [])
 
     React.useEffect(() => {
         api.getInitialCards()
@@ -32,15 +34,15 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
         <main className="content">
             <div className="profile">
                 <div className="profile__avatar-wrapper" onClick={onEditAvatar}>
-                    <img src={userAvatar} alt="Жак-Ив Кусто" className="profile__ellipse"/>
+                    <img src={currentUser.avatar} alt="Жак-Ив Кусто" className="profile__ellipse"/>
                     <button className="profile__ellipse-pencil" type="button"></button>
                 </div>
                 <div className="profile__info">
-                    <h1 className="profile__info-name">{userName}</h1>
+                    <h1 className="profile__info-name">{currentUser.name}</h1>
                     <button type="button" className="profile__info-edit-button" onClick={onEditProfile}>
                         <img src={pencil} alt="Карандаш" className="profile__pencil"/>
                     </button>
-                    <p className="profile__info-text">{userDescription}</p>
+                    <p className="profile__info-text">{currentUser.about}</p>
                 </div>
                 <button type="button" className="profile__add-button" onClick={onAddPlace}></button>
             </div>
