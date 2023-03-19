136f13f3532ff6f01dfb8b3eb5636e4e3447e5fc
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-03-19 15:17:00 +0200

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy; 2022 Анастасия Муравьева</p>
-    </footer>
-  )
-}
-
-export default Footer
